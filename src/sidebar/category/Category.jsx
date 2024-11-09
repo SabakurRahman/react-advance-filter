62a18296a61debd3cbce6581811fbb071670982a
SabakurRahman <sabakur12360@gmail.com>
2024-11-09 22:11:29 +0600

    category component

--- src/sidebar/category/Category.jsx
+++ src/sidebar/category/Category.jsx
@@ -1,8 +1,22 @@
 import "./Category.css";
 const Category = () => {
   return (
-    <div className="category">
-      <h1>Category</h1>
+    <div>
+      <h2 className="sidebar-title">Category</h2>
+      <div>
+        <label className="sidebar-label-container">
+          <input type="radio" name="test" />
+          <span className="checkmark"></span>Snekers
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name="test" />
+          <span className="checkmark"></span>Flats
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name="test" />
+          <span className="checkmark"></span>Sandals
+        </label>
+      </div>
     </div>
   );
 };
